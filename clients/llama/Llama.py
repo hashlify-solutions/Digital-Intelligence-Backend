a077332eebb8bcc2195cbe8e6f24d7d92b9fb133
Abdul Raffay <raffay.ahmed009@gmail.com>
2026-03-02 00:54:16 +0500

    update: langchain imports v2

--- clients/llama/Llama.py
+++ clients/llama/Llama.py
@@ -1,5 +1,5 @@
 from langchain_ollama import ChatOllama
-from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 import json
 import re
--- clients/llama/llama_v1.py
+++ clients/llama/llama_v1.py
@@ -1,5 +1,5 @@
 from langchain_ollama import ChatOllama
-from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from numpy import isin
 from setup import setup_logging
